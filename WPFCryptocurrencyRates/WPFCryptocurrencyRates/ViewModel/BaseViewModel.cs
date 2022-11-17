36ccf42bd67a11f5a53034eb06e171f6c570ec7c
OsmanMazinov <wakahackaflame@gmail.com>
2022-11-17 02:17:26 +0100

    Add project files.

---
+++ WPFCryptocurrencyRates/WPFCryptocurrencyRates/ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WPFCryptocurrencyRates.Annotations;
+
+namespace WPFCryptocurrencyRates.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+}
